390828c468a2cbf311a3cdc8f6b63b984e2053f9
AnmolGarampalli27 <anmolgarampalli@gmail.com>
2024-12-15 13:00:18 +0530

    Radio button & checkbox interaction

--- cypress/e2e/RadioButtons&Checkboxes.cy.js
+++ cypress/e2e/RadioButtons&Checkboxes.cy.js
@@ -1,12 +1,23 @@
-/// <reference types = "Cypress" >
+/// < reference types="Cypress" />
+
 
 describe("Check UI elements", ()=>{
     it("Checking Radio Buttons",()=>{
 
-       
+cy.visit ("https://qa-automation-practice.netlify.app/radiobuttons")
+cy.get("label[for='radio-button2']").should('be.present');
 
-  
+it('Checkbox interaction', () => {
 
+    cy.visit ("https://formstone.it/components/checkbox/")
+cy.get("div[class='fs-checkbox fs-light fs-checkbox-checked'] div[class='fs-checkbox-flag']").check();    
+
+});
+
+
+// This module is still pending to be completed
+//Moving further to next module
 
     })
-}  )
+}  
+)
