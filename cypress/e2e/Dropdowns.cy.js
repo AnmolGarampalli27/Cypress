c17d533ad21f1ed57e8768d28bc8d6a02c5067f5
AnmolGarampalli27 <anmolgarampalli@gmail.com>
2024-12-15 20:27:37 +0530

    Learning dropdown interaction

--- cypress/e2e/Dropdowns.cy.js
+++ cypress/e2e/Dropdowns.cy.js
@@ -1,5 +1,5 @@
 describe("Dropdown interaction", ()=>{
-it('Dropdown', () => {
+it.skip('Dropdown', () => {  // The skip attribute helps in skipping the block for the next execution phase.
     
 cy.visit("https://www.zoho.com/commerce/free-demo.html");
 
@@ -7,18 +7,16 @@ cy.get("#zcf_address_country"). //locating the element
 select("Hong Kong"). //selecting the value
 should('have.value','Hong Kong') //verifying the value
 
+})
+it("dropdown without select, using click", () => {
+    cy.visit("https://www.dummyticket.com/dummy-ticket-for-visa-application");
+    cy.get("#select2-billing_country-container").click();
 
+    cy.get("input[role='combobox']").type("Italy").type("{enter}");
+    
+    cy.get("#select2-billing_country-container").
+    should('have.text','Italy'); //validating the selected value in the dropdown
+    // have.value assertion cannot be used to compare value because we are filling in text format  
 
-
-
-});    
-
-
-
-
-}
-
-
-
-
-)
+})   
+})
