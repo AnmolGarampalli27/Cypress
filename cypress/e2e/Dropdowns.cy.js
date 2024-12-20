238b65f01d20756b8536b091c9b5c71ed6f7f84d
AnmolGarampalli27 <anmolgarampalli@gmail.com>
2024-12-21 00:23:41 +0530

    Adding dropdown interaction changes

--- cypress/e2e/Dropdowns.cy.js
+++ cypress/e2e/Dropdowns.cy.js
@@ -8,15 +8,45 @@ select("Hong Kong"). //selecting the value
 should('have.value','Hong Kong') //verifying the value
 
 })
-it("dropdown without select, using click", () => {
+//This block of code is used to select the value from the dropdown using the select method.
+// The below code enters  the value to be selected & verifies the selected value.
+
+it.skip("dropdown without select, using click", () => {
     cy.visit("https://www.dummyticket.com/dummy-ticket-for-visa-application");
     cy.get("#select2-billing_country-container").click();
 
-    cy.get("input[role='combobox']").type("Italy").type("{enter}");
+    cy.get("input[role='combobox']").type("Japan").type("{enter}");
     
     cy.get("#select2-billing_country-container").
-    should('have.text','Italy'); //validating the selected value in the dropdown
+    should('have.text','Japan'); //validating the selected value in the dropdown
     // have.value assertion cannot be used to compare value because we are filling in text format  
 
-})   
+}) 
+it.skip ( 'Handing auto suggested dropdown', () => {
+
+    // navigating to the wikipedia page
+    cy.visit("https://www.wikipedia.org/");
+    // Locating the search box
+    cy.get("#searchInput").type("Delhi");
+    // Adding text to the search box
+    // suggestions are displayed
+    cy.get(".suggestion-link").contains("Delhi University").click();
+    // selecting the suggestion
+
+    cy.url().should('include','Delhi_University');
+    // validating the selected suggestion
+
+})
+
+it("Handling dynamic dropdown", () => {
+    // Add your test code here
+    // Navigate to the google page
+    cy.visit("https://www.google.co.in/");
+
+    // Locating the search box
+    cy.get("[name='q']").type("Cypress Automation");
+    // Adding text to the search box
+    // suggestions are displayed
+})
+
 })
